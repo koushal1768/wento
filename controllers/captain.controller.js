9f03f9c15d85dca6c7d34f5ff86601782c99396c
Koushal Patel <151174225+koushal1768@users.noreply.github.com>
2026-08-08 00:17:19 +0530

    update the captain controller

--- controllers/captain.controller.js
+++ controllers/captain.controller.js
@@ -1,6 +1,6 @@
 const captainModel = require('../models/captain.model');
 const captainService = require('../services/captain.service');
-const blackListTokenModel = require('../models/blacklistToken.model');
+const blackListTokenModel = require('../models/blackListToken.model');
 const { validationResult } = require('express-validator');
 
 
@@ -79,4 +79,3 @@ module.exports.logoutCaptain = async (req, res, next) => {
 
     res.status(200).json({ message: 'Logout successfully' });
 }
-
